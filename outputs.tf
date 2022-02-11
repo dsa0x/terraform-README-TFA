0f9fc8e33f1e0dd720b943b58688b4cc7d1557d4
Samsondeen <40821565+dsa0x@users.noreply.github.com>
2022-02-11 11:42:59 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "ec2_instance_public_ips" {
   description = "Public IP addresses of EC2 instances"
   value       = module.ec2_instances.public_ip
 }
+
+  output "default_max_pods_per_node" {
+  description = "default_max_pods_per_node"
+  value       = var.default_max_pods_per_node
+}
